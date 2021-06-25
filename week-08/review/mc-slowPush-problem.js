110a4dae391f3a6922edb4366e64f8d60f398841
Soon-Mi Sugihara <ssoonmi@gmail.com>
2021-06-25 19:35:18 -0400

    Add better comments to slowPush problem

--- week-08/review/mc-slowPush-problem.js
+++ week-08/review/mc-slowPush-problem.js
@@ -10,9 +10,9 @@ function slowPush(arr, val, delay) {
 
 const arr = [];
 
-slowPush(arr, 1, 1000) // [1]
-  .then(() => slowPush(arr, 2, 2000)) // [1, 2]
-  .then(() => slowPush(arr, 3, 3000)); // [1, 2, 3]
+slowPush(arr, 1, 1000) // after 1 second: arr = [1]
+  .then(() => slowPush(arr, 2, 2000)) // after 2 more seconds: arr = [1, 2]
+  .then(() => slowPush(arr, 3, 3000)); // after 3 more seconds: arr = [1, 2, 3]
 
 // added a setTimeout that will log `arr` to the console after 4 seconds
 setTimeout(() => {
